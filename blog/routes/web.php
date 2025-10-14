beb616097ef4b2a1d294fd09b8393474fdfcc874
Amine mohmed <aminmohamedaboamasha@gmail.com>
2025-10-14 12:22:22 +0100

    commit changes in mvc branch

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -2,6 +2,7 @@
 
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,21 @@ use Illuminate\Support\Facades\Route;
 // routes/web.php
 
 Route::get('/ping', fn() => 'pong');
+
+
+// routes/web.php
+
+
+
+
+
+// Route test
+//Route::get('/ping', fn() => 'pong');
+
+// Routes nommées avec contrôleur
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/a-propos', [PageController::class, 'about'])->name('about');
+
+// Mini-routes articles (mockées pour l’instant)
+Route::get('/articles', [PageController::class, 'articles'])->name('articles.index');
+Route::get('/articles/{slug}', [PageController::class, 'show'])->name('articles.show');
